c7be711bc63614038bb9d870d295811f4570fa5b
Sabhari <128909840+sabhari-krr@users.noreply.github.com>
2023-09-14 22:20:13 +0530

    Update updateApproval.php

--- updateApproval.php
+++ updateApproval.php
@@ -1,20 +1,18 @@
 <?php
 include "config.php";
 
-if (isset($_POST['yeah']))
-{
+if (isset($_POST['yeah'])) {
     $id = $_POST["id"];
     $query = "UPDATE approval SET exitApproval = 1 WHERE id = '$id'";
     $result = mysqli_query($db, $query);
     if ($result) {
         $res = [
             'status' => 200,
-            'message' => 'Updated Successfully'
+            'message' => 'Hope you had a nice meeting 😃'
         ];
         echo json_encode($res);
-            return;
-    }
-    else{
+        return;
+    } else {
         $res = [
             'status' => 500,
             'message' => 'Details Not updated, Give relevant input'
@@ -22,8 +20,7 @@ if (isset($_POST['yeah']))
         echo json_encode($res);
         return;
     }
-}
-else{
+} else {
     $res = [
         'status' => 500,
         'message' => 'If not even entered'
