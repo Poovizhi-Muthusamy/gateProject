f02d0b7aad36e256a0878f674dd7a2c98b40f715
Sabhari <128909840+sabhari-krr@users.noreply.github.com>
2023-09-14 09:17:42 +0530

    Create updateApproval.php

---
+++ updateApproval.php
@@ -0,0 +1,33 @@
+<?php
+include "config.php";
+
+if (isset($_POST['yeah']))
+{
+    $id = $_POST["id"];
+    $query = "UPDATE approval SET exitApproval = 1 WHERE id = '$id'";
+    $result = mysqli_query($db, $query);
+    if ($result) {
+        $res = [
+            'status' => 200,
+            'message' => 'Updated Successfully'
+        ];
+        echo json_encode($res);
+            return;
+    }
+    else{
+        $res = [
+            'status' => 500,
+            'message' => 'Details Not updated, Give relevant input'
+        ];
+        echo json_encode($res);
+        return;
+    }
+}
+else{
+    $res = [
+        'status' => 500,
+        'message' => 'If not even entered'
+    ];
+    echo json_encode($res);
+    return;
+}
